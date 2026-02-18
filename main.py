2f134c7a830a47440db0acb79c36f1ee5c829e50
esobirjon2197 <esobirjon2197@gmail.com>
2026-02-18 16:37:42 +0500

    Add set operations examples in main.py
    This script demonstrates various set operations in Python, including intersection, union, and removal of elements. It also shows how to convert a list to a set to remove duplicates.

---
+++ main.py
@@ -0,0 +1,45 @@
+# 1-m
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+kesishma = A & B
+print(kesishma)
+
+
+# 2-m
+A = {10, 20, 30}
+print(A)
+B = {30, 40, 50}
+print(B)
+
+birikma = A | B
+print(birikma)
+
+
+# 3-m
+numbers = {5, 10, 15, 20, 25}
+
+print(numbers)
+
+print(len(numbers))
+
+
+# 4-m
+numbers = {100, 200, 300, 400}
+
+print(numbers)
+
+numbers.remove(300)
+print(numbers)
+
+
+# 5-m
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+print(numbers)
+
+numbers = set(numbers)
+print(numbers)
+
+
+
